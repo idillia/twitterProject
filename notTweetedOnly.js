23d65cf620f47c4469d211209d2319c8843d3324
idillia <mila.antonova@gmail.com>
2017-01-10 09:31:56 -0800

    add tweeted data to diffrence function

--- notTweetedOnly.js
+++ notTweetedOnly.js
@@ -5,9 +5,10 @@ var fs = require('fs');
 var twitterHandlers = require('./json/twitterHandlers');
 
 var already_tweeted_993 = require('./json/already_tweeted_993');
+var already_tweeted_980 = require('./json/already_tweeted_980');
 
 
-var uniqTwitterHandlers = _.difference(twitterHandlers, already_tweeted_993);
+var uniqTwitterHandlers = _.difference(twitterHandlers, already_tweeted_993, already_tweeted_980.json);
 
     fs.writeFile('./json/uniqTwitterHandlers.json', JSON.stringify(uniqTwitterHandlers, null, 2), function (err) {
       if (err) return console.log(err);
